819d198cd9c039df556f56288357853e27b4b81d
Luke Bassett <bassettl@wit.edu>
2022-11-14 12:38:23 -0500

    first commit

---
+++ Pokemon-Final-Project/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::setprecision;
+using std::string;
+using std::ofstream;
+using std::ifstream;
+using std::endl;
+
+int main() {
+
+
+
+	return 0;
+}
